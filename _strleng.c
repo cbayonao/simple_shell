bb3d69e86d8e7a590976c9a5467fb8f3d28e362e
juankarlos999 <juan_karlos_999@hotmail.com>
2019-11-16 12:37:19 -0500

    Funtion that return size of array

---
+++ _strleng.c
@@ -0,0 +1,14 @@
+/**
+ * _strleng -Function that move between of array and store his size
+ * @str: pointer string
+ * Return: size of string
+ **/
+int _strleng(char *str)
+{
+	int i;
+
+	i = 0;
+	for (; str[i] != '\0'; i++)
+	{}
+return (i);
+}

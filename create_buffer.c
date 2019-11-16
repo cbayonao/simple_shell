74f40d808ec8032782869d85d51f8d1fa2edbe39
juankarlos999 <juan_karlos_999@hotmail.com>
2019-11-16 12:30:36 -0500

    Create buffer

---
+++ create_buffer.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * create_buffer - Funtion create a buffer en heap of 1024 bytes
+ * Return: Buffer
+ **/
+char *create_buffer(void)
+{
+	char *buffer;
+
+	buffer = malloc(1024 * sizeof(char));
+	if (buffer == NULL)
+		exit(1);
+	*buffer = '\0';
+return (buffer);
+}
+

0e32c7275f197de53c5a315e567602f638d836a4
juankarlos999 <juan_karlos_999@hotmail.com>
2019-11-17 14:30:12 -0500

    Prototipe funtion initial shell

---
+++ start_shell.c
@@ -0,0 +1,25 @@
+#include <shell.h>
+/**
+ * start_shell -funtion that runs the shell
+ * Return : None
+ **/
+void start_shell(void)
+{
+	char *buffer;
+	size_t bufsize = 64;
+	size_t char_input;
+
+	welcome_shell();
+	buffer = create_buffer();
+	_printf("$ ");
+	char_input = getline(&buffer, &bufsize, stdin);
+	if (char_input == EOF)
+	{
+		free(buffer);
+		exit(1);
+	}
+	check_arg(buffer);
+	free(buffer);
+	start_shell();
+}
+

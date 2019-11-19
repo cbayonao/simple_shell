596d24255cc3c41eb57aae0734ef48816a10f136
juankarlos999 <juan_karlos_999@hotmail.com>
2019-11-19 08:35:04 -0500

    Main and funtions prototype

---
+++ main.c
@@ -0,0 +1,99 @@
+#include "shell.h"
+void start_shell();
+void check_arg(char *str);
+void split_str (char *s1);
+void new_Pdi(char *arg);
+//void _execve(char *arg);
+/*
+ * main -funtion principal init program simple shell
+ * Return: Zero for Succes
+ */
+int main(int argc, char *argv[]) 
+{
+	welcome_shell();
+
+	if (argc > 1)
+	{
+	int j = 1;
+	for (int i = argc - 1; i > 0; i--, j++)
+	//	_printf(argv[j]);
+		split_str(argv[j]);
+	}
+	else
+		start_shell();
+
+   return (0);
+} 
+
+void start_shell()
+{
+	char *buffer;
+	size_t bufsize = 64;
+	size_t char_input;
+
+
+	buffer = create_buffer();
+	_printf("$ ");
+	char_input = getline(&buffer, &bufsize, stdin);
+	if (char_input == EOF)
+	{
+		free(buffer);
+		exit(1);
+	}
+//	check_arg(buffer);
+//	_execve(buffer);
+	new_Pdi(buffer);
+	free(buffer);
+	start_shell();
+}
+
+void check_arg(char *str)
+{
+	if (strcmp(str, "ls\n") == 0)
+	{
+		_printf("No such file or directory\n");
+	}
+	else if (strcmp(str, "exit\n") == 0)
+	{
+		exit(1);
+	}
+
+}
+
+void split_str(char *s1)
+{
+   char s2[] = " ,";
+   char *ptr;
+
+	ptr = strtok( s1, s2 );
+	if (ptr != NULL)
+	{
+		while(ptr != NULL)
+		{
+			printf("%s", ptr);
+		ptr = strtok(NULL, s2);
+		}
+	}
+}
+
+void new_Pdi(char *arg)
+{
+	char *str[2];
+	int status, j;
+	pid_t id_pdi;
+
+	id_pdi = fork();
+	str[0] = arg;
+	str[1] = NULL;
+	if (id_pdi == 0)
+	{
+		_printf("sub process successful\n");
+		_printf(arg);
+		execve(str[0], str, NULL);
+		sleep(2);
+		exit(1);
+	}
+	id_pdi = wait(&status);
+	if ((WIFEXITED(status)) > 0)
+		_printf("End process son. Return to process father\n");
+}

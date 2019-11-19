ea9f4745af27d6a1d32e15d0656b905c3a38a943
juankarlos999 <juan_karlos_999@hotmail.com>
2019-11-19 15:55:48 -0500

    [Upgrade]Print characters

--- _printf.c
+++ _printf.c
@@ -4,14 +4,11 @@
  * @str: pointer with information from buffer
  * Return: type data ssize_t
  **/
-int _printf(char *str)
+void _printf(char *str)
 {
-	ssize_t write_file;
 	size_t len;
 
 	len = _strleng(str);
-	write_file = write(1, str, len);
-
-return (write_file);
+	write(1, str, len);
 }
 

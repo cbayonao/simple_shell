c7b07cbbf152d80d9962bf36ea8c529a56b9ada6
juankarlos999 <juan_karlos_999@hotmail.com>
2019-11-27 18:19:03 -0500

    [update]main

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include "shell.h"
 void start_shell();
-void split_str (char *s1);
 /*
  * main -funtion principal init program simple shell
  * Return: Zero for Succes
  */
 int main(int argc, char *argv[]) 
 {
-	welcome_shell();
 
 	if (argc > 1)
 	{
 	int j = 1;
 	for (int i = argc - 1; i > 0; i--, j++)
-		check_arg(argv[j]);
+	{
+		_printf(argv[j]);
+		perror(" ERROR-");
+		exit(1);
+	}
 	}
 	else
+	{
+		welcome_shell();
 		start_shell();
+	}
 
    return (0);
 } 
@@ -25,33 +30,22 @@ void start_shell()
 {
 	char *buffer;
 	size_t bufsize = 64;
-	int char_input;
+	int aux;
+
+	signal(SIGINT, handle_sigint);
 
 	buffer = create_buffer();
 	_printf("$ ");
-	char_input = getline(&buffer, &bufsize, stdin);
-	if (char_input == EOF)
+	aux = getline(&buffer, &bufsize, stdin);
+
+	if (aux == EOF)
 	{
 		free(buffer);
 		exit(1);
 	}
+//	exec_proc(buffer);
 	check_arg(buffer);
+//	split_str(buffer);
 	free(buffer);
 	start_shell();
-}
-
-void split_str(char *s1)
-{
-   char s2[] = " ,";
-   char *ptr;
-
-	ptr = strtok( s1, s2 );
-	if (ptr != NULL)
-	{
-		while(ptr != NULL)
-		{
-			printf("%s", ptr);
-		ptr = strtok(NULL, s2);
-		}
-	}
 }
